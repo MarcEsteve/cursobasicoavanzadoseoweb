05b114d5f6d2816d683f9c18c0b8cb7ac4cc5116
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2022-10-18 17:26:51 +0200

    rectificacion

--- JS/dias-semana.js
+++ JS/dias-semana.js
@@ -1,24 +1,36 @@
-var dia;
-switch (new Date().getDay()) {
+// Condicional SWITCH
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+var dia=new Date().getDay();
+var mes=new Date().getMonth();
+var fecha=new Date();
+var numdia=dia;
+switch (dia) {
     case 0:
-    dia = "Domingo"; 
+        dia = "Domingo"; 
     break;
     case 1:
-    dia = "Lunes"; 
+        dia = "Lunes"; 
     break;
     case 2:
-    dia = "Martes"; 
+        dia = "Martes"; 
     break;
     case 3:
-    dia = "Miércoles"; 
+        dia = "Miércoles"; 
     break;
     case 4:
-    dia = "Jueves"; 
+        dia = "Jueves"; 
     break;
     case 5:
-    dia = "Viernes"; 
+        dia = "Viernes"; 
     break;
     case 6:
-    dia = "Sábado";
+        dia = "Sábado";
 }
-document.getElementById("texto").innerHTML = "Hoy es " + dia;
+document.getElementById("texto").innerHTML = "Hoy es " + dia + ", el valor del dia 'case' es " + numdia + "<br>Muestra la fecha: " + fecha + mes;
