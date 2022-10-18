47efd07d26e1ed242074d4da2b4cc6d1981acb61
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2022-10-18 18:12:29 +0200

    más ejemplos array

--- JS/matriz-array.js
+++ JS/matriz-array.js
@@ -1,2 +1,10 @@
 var bici = ["BH", "Orbea", "Specialized"];
-document.getElementById("demo").innerHTML = "Mi nueva bici es: " + bici[2];
+document.getElementById("demo").innerHTML = "Mi nueva bici es: " + bici[3];
+bici[3]="Decathlon";
+document.getElementById("demo2").innerHTML = "Mi nueva bici es: " + bici[3];
+document.getElementById("demo3").innerHTML = "Mi nueva bici es: " + bici[1];
+bici[1]="OTRA";
+document.getElementById("demo4").innerHTML = "Mi nueva bici es: " + bici[1];
+//Ejemplos de valores de array
+var array = [ 3, "hola", true, "bona nit"  ]
+var nombre = [1, "Marc", "Esteve", "Torrelles"]
