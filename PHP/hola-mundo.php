6110ba8082d06bd21039f63166561446a063cb6a
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2022-10-21 16:48:47 +0200

    primer PHP

---
+++ PHP/hola-mundo.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <!-- Código de PHP -->
+        <?php
+            // Declarar y asignar variables PHP
+            $x = "Hola mundo";
+            $y = 5;
+            $z = 10.5;
+            //Mostrar en pantalla con "echo"
+            echo $x;
+            echo "<br>";
+            echo $y;
+            echo "<br>";
+            echo $z;
+        ?>
+    </body>
+</html>
